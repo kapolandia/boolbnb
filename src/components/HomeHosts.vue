00ac08d4c7638a433942889c21659bf8475ee798
kapolandia <paykapo4@gmail.com>
2024-06-27 20:30:33 +0200

    some ui changes to slider

--- src/components/HomeHosts.vue
+++ src/components/HomeHosts.vue
@@ -8,20 +8,30 @@
     <div class="container text-center justify-content-center mt-5 pt-4">
         <h2 class="fw-bold">Con i migliori host <i class="fa-solid fa-heart primary-color"></i></h2>
         <div class="slider mt-4" style="
-            --width: 200px;
+            --width: 100px;
             --height: 100px;
             --quantity: 10;
         ">
             <div class="list">
-                <div class="item" style="--position: 1"><img src="../assets/images/profile_1.webp" alt=""></div>
-                <div class="item" style="--position: 2"><img src="../assets/images/profile_2.webp" alt=""></div>
-                <div class="item" style="--position: 3"><img src="../assets/images/profile_3.webp" alt=""></div>
-                <div class="item" style="--position: 4"><img src="../assets/images/profile_4.webp" alt=""></div>
-                <div class="item" style="--position: 5"><img src="../assets/images/profile_5.webp" alt=""></div>
-                <div class="item" style="--position: 6"><img src="../assets/images/profile_6.webp" alt=""></div>
-                <div class="item" style="--position: 7"><img src="../assets/images/profile_7.webp" alt=""></div>
-                <div class="item" style="--position: 8"><img src="../assets/images/profile_8.webp" alt=""></div>
-                <div class="item" style="--position: 9"><img src="../assets/images/profile_9.webp" alt=""></div>
+                <div class="item" style="--position: 1">
+                    <img src="../assets/images/profile_1.webp" alt="">
+                </div>
+                <div class="item" style="--position: 2">
+                    <img src="../assets/images/profile_2.webp" alt=""></div>
+                <div class="item" style="--position: 3">
+                    <img src="../assets/images/profile_3.webp" alt=""></div>
+                <div class="item" style="--position: 4">
+                    <img src="../assets/images/profile_4.webp" alt=""></div>
+                <div class="item" style="--position: 5">
+                    <img src="../assets/images/profile_5.webp" alt=""></div>
+                <div class="item" style="--position: 6">
+                    <img src="../assets/images/profile_6.webp" alt=""></div>
+                <div class="item" style="--position: 7">
+                    <img src="../assets/images/profile_7.webp" alt=""></div>
+                <div class="item" style="--position: 8">
+                    <img src="../assets/images/profile_8.webp" alt=""></div>
+                <div class="item" style="--position: 9">
+                    <img src="../assets/images/profile_9.webp" alt=""></div>
                 <div class="item" style="--position: 10"><img src="../assets/images/profile_10.webp" alt=""></div>
             </div>
         </div>
@@ -66,7 +76,10 @@
     // Slider
     .slider{
         width: 100%;
-        height: var(--height);
+        height:200px;
+        padding-top: 30px;
+        padding-bottom: 100px;
+        margin-bottom: 100px;
         overflow: hidden;
         mask-image: linear-gradient(
             to right,
@@ -88,10 +101,15 @@
         left: 100%;
         animation: autoRun 10s linear infinite;
         transition: filter 0.5s;
+        border-radius: 12px;
+        overflow: hidden;
         animation-delay: calc( (10s / var(--quantity)) * (var(--position) - 1) )!important;
     }
-    .slider .list .item img{
+    .slider .list .item img {
         width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
     }
     @keyframes autoRun{
         from{
@@ -106,15 +124,15 @@
     }
     .slider .item:hover{
         filter: grayscale(0);
+        transform: scale(1.5); /* Scale up the item on hover */
+        background-color: #fff;
+        z-index: 10; /* Ensure the hovered item is above other items */
+        border-radius: 12px;
+        overflow: hidden;
+        position: relative;
     }
+
     .slider[reverse="true"] .item{
         animation: reversePlay 10s linear infinite;
-    }
-    @keyframes reversePlay{
-        from{
-            left: calc(var(--width) * -1);
-        }to{
-            left: 100%;
-        }
     }
 </style>
