565e102277a6ee23c2917dc23c6155676ce9bf1f
DamianoSpampinato <ghgg585@gmail.com>
2024-06-30 16:06:01 +0200

    feat: add share pop up init

--- src/components/ApartmentList.vue
+++ src/components/ApartmentList.vue
@@ -1,13 +1,19 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import ShareProp from './ShareProp.vue';
+
 export default {
     name: 'ApartmentList',
+    components: {
+        ShareProp
+    },
     data() {
         return {
             apiUrl: 'http://127.0.0.1:8000/api',
             hosts:[],
             //caricamento api
             hostIsLoading: true,
+            Popup: {},
         }
     },
 
@@ -20,9 +26,14 @@ export default {
                 this.hosts = response.data.results;
                 console.log('minchia quante api');
                 this.hostIsLoading= false;
-                console.log(this.hosts);
+                
+            });
+        },
+        GetPopup(host, event){
+            console.log(host);
             
-        })
+            this.Popup = host;
+            
         }
     },
     mounted(){
@@ -33,43 +44,50 @@ export default {
 
 <template>
     <!-- display solo se il caricamento è finito -->
-    <div 
-    v-if="!hostIsLoading"
-    class="container text-center mt-4">
-    <div class="my-border-bottom mb-5">
-        <h2 class="fw-bold">Per tutti i gusti <i class="fa-solid fa-house-circle-check primary-color"></i></h2>
-        <p class="p-subtitle w-75 mx-auto">Che tu stia cercando un accogliente appartamento in centro, una villa spaziosa con giardino, o una tranquilla casa di campagna, siamo certi che abbiamo la soluzione giusta per te.</p>
-    </div>
-        <div class="row">
-            <div class="col-3 my-2">
-                <div class="ms-card">
-                    <div class="img-container">
-                        <img src="../assets/images/image.png" alt="">
-                    </div>
-                <h5>Ciao</h5>
-                <div>100€ a notte</div>
-                </div>
+     <section class="position relative">
+
+         <div 
+         v-if="!hostIsLoading"
+         class="container text-center mt-4">
+         <div class="my-border-bottom mb-5">
+             <h2 class="fw-bold">Per tutti i gusti <i class="fa-solid fa-house-circle-check primary-color"></i></h2>
+             <p class="p-subtitle w-75 mx-auto">Che tu stia cercando un accogliente appartamento in centro, una villa spaziosa con giardino, o una tranquilla casa di campagna, siamo certi che abbiamo la soluzione giusta per te.</p>
             </div>
-            <div
-            v-for="host in hosts"
-            class="col-3 my-2">
-            <router-link class="text-decoration-none text-black" :to="{name: 'host-show', params: {'slug' : host.slug}}">
-                <div class="ms-card">
+            <div class="row">
+                <div class="col-3 my-2">
+                    <div class="ms-card">
+                        <div class="img-container">
+                            <img src="../assets/images/image.png" alt="">
+                        </div>
+                        <h5>Ciao</h5>
+                        <div>100€ a notte</div>
+                    </div>
+                </div>
+                <div
+                v-for="host in hosts"
+                class="col-3 my-2">
+                <router-link class="text-decoration-none text-black position-relative" :to="{name: 'host-show', params: {'slug' : host.slug}}">
+                    <div class="ms-card">
                         <div class="img-container position-relative my-2">
-                            <div class="share-button p-2 rounded-circle position-absolute"><i class="fa-solid fa-arrow-up-from-bracket"></i></div>
+                            <div @click="GetPopup(host)" class="share-button p-2 rounded-circle position-absolute"><i class="fa-solid fa-arrow-up-from-bracket"></i></div>
                             <img src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNjE4NTg5MzIzNjI0NjI2MA%3D%3D/original/55a2777a-fc80-4d7b-af05-a9f737ff8661.jpeg?im_w=1440&im_q=highq" alt="">
                         </div>
-                    <h5>{{ host.title }}</h5>
-                    <div>{{ host.price }}€/notte</div>
+                        <h5>{{ host.title }}</h5>
+                        <div>{{ host.price }}€/notte</div>
+                        <ShareProp :shareProp="host"></ShareProp>
+                    </div>
+                </router-link>
                 
-                </div>
-            </router-link>
             </div>
         </div>
     </div>
+</section>
 </template>
 
 <style lang="scss">
+router-link{
+z-index: 0;
+}
 .ms-card{
     display: flex;
     flex-direction: column;
@@ -82,6 +100,7 @@ export default {
         top:10px;
         right: 10px;
         background-color: rgba($color: lightgrey, $alpha: 0.7);
+        z-index: 15616516;
     }
     .img-container{
         width: 100%;
---
+++ src/components/ShareProp.vue
@@ -0,0 +1,38 @@
+<script>
+export default  {
+    name: 'ShareProp',
+    props: {
+        shareProp: Object,
+  },
+  data(){
+    return{
+        
+    }
+  } 
+}
+</script>
+
+<template>
+
+<div class="container popup d-flex align-items-center justify-content-center">
+    {{ shareProp.title }}
+    {{$route.fullPath + 'hosts/' + shareProp.slug }} 
+</div>
+</template>
+
+<style lang="scss" scoped>
+.container{
+    background-color: rgba($color: lightgrey, $alpha: 0.7);
+    height: 60vh;
+    width: 70vh;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50% , 50% );
+    opacity: 0;
+    &.popup{
+        opacity: 1;
+    }
+}
+
+</style>
