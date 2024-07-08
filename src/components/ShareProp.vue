66216bbe4f130f24a0f80f5f98521f6e6d0d604b
DamianoSpampinato <ghgg585@gmail.com>
2024-07-08 15:35:06 +0200

    fix container popup

--- src/components/ShareProp.vue
+++ src/components/ShareProp.vue
@@ -76,7 +76,7 @@ export default  {
     background-color: white;
     
     width: 50%;
-    position: absolute;
+    position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50% , -50% );
